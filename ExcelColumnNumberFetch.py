ae71277320995b93d7e31bb0fa395133c2bb43aa
Zain Siddiqui <32204330+zain2511@users.noreply.github.com>
2018-06-20 13:03:49 -0700

    added comments

--- ExcelColumnNumberFetch.py
+++ ExcelColumnNumberFetch.py
@@ -4,8 +4,8 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        s = s[::-1]
+        s = s[::-1]     #inverting the string
         sum = 0;
         for i,x in enumerate(s):
-            sum += (ord(x) - 64) * (26**i)
+            sum += (ord(x) - 64) * (26**i)      #fetching the numeric value and multiplying it with base 26**index
         return sum
